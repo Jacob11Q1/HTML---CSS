e6ad50f90dc9613fdd53f3d8e040b185846a8c56
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2026-01-17 19:31:29 +0200

    Started with JavaScript

---
+++ FrontEnd Path/JavaScript/Building a passenger counter app/script.js
@@ -0,0 +1,9 @@
+// document.getElementById("count-el").innerText = 5;
+// let myAge = 29;
+// console.log(myAge);
+
+let firstBatch = 5;
+let secondBatch = 7;
+
+let count = firstBatch + secondBatch;
+console.log(count);
