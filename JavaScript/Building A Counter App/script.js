f5bf56679eeed789b0a0c1e2986c7bca6fbbeec8
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2026-03-10 19:10:56 +0200

    Basic JS Mathematical Operations

--- JavaScript/Building A Counter App/script.js
+++ JavaScript/Building A Counter App/script.js
@@ -1,7 +1,20 @@
 // document.getElementById("count-el").innerText = 5;
 
-let count = 0;
-console.log(count);
+// let count = 0;
+// console.log(count);
 
+// Basic Mathematical operations:
+// let firstBatch = 7;
+// let secondBatch = 9;
+// let count = firstBatch / secondBatch;
+// console.log("Basic Mathematical Operations: " + count);
+// let myAge = 29;
+// console.log(myAge);
+
+
+// Challenge Dog Age:
 let myAge = 29;
-console.log(myAge);
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log("My Age in Dog Age: " + myDogAge);
+
