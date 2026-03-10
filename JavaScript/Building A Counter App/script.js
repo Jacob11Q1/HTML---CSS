0306a8b5c9e563794f447032397e6665f0395bea
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2026-03-10 19:34:12 +0200

    Reassigning and incramenting

--- JavaScript/Building A Counter App/script.js
+++ JavaScript/Building A Counter App/script.js
@@ -13,8 +13,30 @@
 
 
 // Challenge Dog Age:
-let myAge = 29;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
-console.log("My Age in Dog Age: " + myDogAge);
+// let myAge = 29;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log("My Age in Dog Age: " + myDogAge);
 
+// Reassigning and incramenting:
+let count = 5;
+
+// Incrament count by 3:
+count = count + 3;
+count = count + 15;
+console.log("Reassinging and incramenting: " + count);
+
+// Challenge bonusPoint:
+let bonusPoints = 50;
+
+// Incramenting by 50:
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+// Incramenting it to make 25: 
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+// Incramenting it to make 70: 
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
