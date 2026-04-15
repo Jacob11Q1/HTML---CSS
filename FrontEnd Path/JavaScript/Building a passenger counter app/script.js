14894e379c82c5ae9cb5066a6a1aed871e3a88dc
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2026-04-15 20:15:31 +0300

    Started JS Basics

--- FrontEnd Path/JavaScript/Building a passenger counter app/script.js
+++ FrontEnd Path/JavaScript/Building a passenger counter app/script.js
@@ -1,9 +1,28 @@
 // document.getElementById("count-el").innerText = 5;
-// let myAge = 29;
+// let myAge = 30;
 // console.log(myAge);
 
-let firstBatch = 5;
-let secondBatch = 7;
+// let firstBatch = 5;
+// let secondBatch = 8;
+// let count = firstBatch + secondBatch;
+// console.log(count);
 
-let count = firstBatch + secondBatch;
+// Challenge Human To Dog Age Ratio:
+let myAge = 30;
+let humanDogRatio= 7;
+let myDogAge = myAge * humanDogRatio;
+console.log("My Dog Age: " + myDogAge);
+
+// Assigneing the value of count
+let count = 5;
+count = count + 2;
 console.log(count);
+
+// Challenge BonusPoints:
+let bonusPoints = 50;
+bonusPoints = bonusPoints + 50;
+
+bonusPoints = bonusPoints - 75;
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
