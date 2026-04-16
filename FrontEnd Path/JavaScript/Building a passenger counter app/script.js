ad5f4401a429d94f20943ca4b8c2d96deeecbf30
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2026-04-16 20:26:46 +0300

    Started with JS Functions

--- FrontEnd Path/JavaScript/Building a passenger counter app/script.js
+++ FrontEnd Path/JavaScript/Building a passenger counter app/script.js
@@ -7,3 +7,20 @@
 function increment() {
     console.log("The button was clicked");
 }
+
+// setting up the race
+function countdown(){
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
+}
+
+countdown()
+
+function declareTheNumber(){
+    console.log(42);
+}
+
+declareTheNumber();
