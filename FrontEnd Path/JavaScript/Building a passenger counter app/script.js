ec2f639871e0041398a0fc83662ffd86ce89c5d8
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2026-04-17 15:42:10 +0300

    Made a change to the counter App

--- FrontEnd Path/JavaScript/Building a passenger counter app/script.js
+++ FrontEnd Path/JavaScript/Building a passenger counter app/script.js
@@ -3,3 +3,12 @@
 // let count = 5;
 // count = count + 1;
 // console.log(count);
+
+let count = 0;
+let countEl = document.getElementById("count-el");
+
+function increment(){
+    count += 1;
+    console.log(count);
+    countEl.innerText = count;
+}
