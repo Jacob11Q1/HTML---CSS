6193642875ffb11623c1ca0822c70aa1407219b5
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2026-04-22 21:31:15 +0300

    Added a new index.html and a new index.js Challenge

---
+++ FrontEnd Path/JavaScript/index.js
@@ -0,0 +1,9 @@
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+let welcomeEl = document.getElementById("welcome-el")
+
+// Create two variables (name & greeting)
+let name = "Your Name"
+let greeting = "Welcome back"
+
+// Render the welcome message using welcomeEl.innerText
+welcomeEl.innerText = greeting + ", " + name
